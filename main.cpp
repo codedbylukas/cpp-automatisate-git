21f90b36c50168883ba4994b386dd56f97f82ac7
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-02-01 09:55:04 -0500

    fixed

--- main.cpp
+++ main.cpp
@@ -7,9 +7,24 @@
 using namespace std;
 bool push = true;
 
+void commit();
+
 int main() {
   init();
   push = configurePush();
-  cout << "push: " << push << endl;
+  while (true) {
+    commit();
+  }
   return 0;
+}
+void commit() {
+  cout << "Whats your commit message?";
+  string message;
+  cin >> message;
+  system("git add .");
+  system(("git commit -m \"" + message + "\"").c_str());
+  if (push) {
+    system("git pull");
+    system("git push");
+  }
 }
