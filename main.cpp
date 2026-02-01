7e6c7fc1277627f4b32739b59146e5bcdcc7e808
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-02-01 09:28:06 -0500

    make default good to use

--- main.cpp
+++ main.cpp
@@ -4,17 +4,20 @@
 #include <string>
 
 using namespace std;
+bool push = true;
 
 int main() {
   init();
   cout << "Do you want to push it after every modyfire? (y/n) (default: y) ";
   string pushing;
-  cin >> pushing;
-  if (pushing == "y") {
+  getline(cin, pushing);
+  if (pushing == "y" || pushing == "") {
     cout << "OK I will push it after every modyfire" << endl;
+    push = true;
   }
   if (pushing == "n") {
     cout << "OK I will not push it after every modyfire" << endl;
+    push = false;
   }
   if (pushing == "q") {
     exit(0);
