a61480fcf846549b8fbb30a7233a5f21b9e9c512
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-02-07 07:54:46 -0500

    add built Linux text

--- automatations/auto_all_compile.sh
+++ automatations/auto_all_compile.sh
@@ -3,6 +3,7 @@ g++ main.cpp components/init.cpp components/commit.cpp components/push.cpp -o ma
 g++ main.cpp components/init.cpp components/commit.cpp components/push.cpp -o Linux/main && \
 x86_64-w64-mingw32-g++ main.cpp components/init.cpp components/commit.cpp components/push.cpp -o windows/64-bit/main.exe  -static-libgcc -static-libstdc++  && \
 i686-w64-mingw32-g++ main.cpp components/init.cpp components/commit.cpp components/push.cpp -o windows/32-bit/main.exe  -static-libgcc -static-libstdc++  && \
+# built Linux
 g++ -E main.cpp -o build/Linux/Preprocessed/main.i && \
 g++ -E components/init.cpp -o build/Linux/Preprocessed/init.i && \
 g++ -E components/commit.cpp -o build/Linux/Preprocessed/commit.i && \
