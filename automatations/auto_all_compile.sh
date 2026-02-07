25931f4dcfb1db4090479c060d8f9b230941ec6d
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-02-07 06:48:02 -0500

    add Linux folder

--- automatations/auto_all_compile.sh
+++ automatations/auto_all_compile.sh
@@ -3,17 +3,17 @@ g++ main.cpp components/init.cpp components/commit.cpp components/push.cpp -o ma
 g++ main.cpp components/init.cpp components/commit.cpp components/push.cpp -o Linux/main && \
 x86_64-w64-mingw32-g++ main.cpp components/init.cpp components/commit.cpp components/push.cpp -o windows/64-bit/main.exe  -static-libgcc -static-libstdc++  && \
 i686-w64-mingw32-g++ main.cpp components/init.cpp components/commit.cpp components/push.cpp -o windows/32-bit/main.exe  -static-libgcc -static-libstdc++  && \
-g++ -E main.cpp -o build/Preprocessed/main.i && \
-g++ -E components/init.cpp -o build/Preprocessed/init.i && \
-g++ -E components/commit.cpp -o build/Preprocessed/commit.i && \
-g++ -E components/push.cpp -o build/Preprocessed/push.i && \
-g++ -S main.cpp -o build/Assembly/main.s && \
-g++ -S components/init.cpp -o build/Assembly/init.s && \
-g++ -S components/commit.cpp -o build/Assembly/commit.s && \
-g++ -S components/push.cpp -o build/Assembly/push.s && \
-g++ -c main.cpp -o build/Objectfile/main.o && \
-g++ -c components/init.cpp -o build/Objectfile/init.o && \
-g++ -c components/commit.cpp -o build/Objectfile/commit.o && \
-g++ -c components/push.cpp -o build/Objectfile/push.o && \
+g++ -E main.cpp -o build/Linux/Preprocessed/main.i && \
+g++ -E components/init.cpp -o build/Linux/Preprocessed/init.i && \
+g++ -E components/commit.cpp -o build/Linux/Preprocessed/commit.i && \
+g++ -E components/push.cpp -o build/Linux/Preprocessed/push.i && \
+g++ -S main.cpp -o build/Linux/Assembly/main.s && \
+g++ -S components/init.cpp -o build/Linux/Assembly/init.s && \
+g++ -S components/commit.cpp -o build/Linux/Assembly/commit.s && \
+g++ -S components/push.cpp -o build/Linux/Assembly/push.s && \
+g++ -c main.cpp -o build/Linux/Objectfile/main.o && \
+g++ -c components/init.cpp -o build/Linux/Objectfile/init.o && \
+g++ -c components/commit.cpp -o build/Linux/Objectfile/commit.o && \
+g++ -c components/push.cpp -o build/Linux/Objectfile/push.o && \
 cd automatations && \
 echo "Compilation complete" 
