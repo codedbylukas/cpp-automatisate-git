5cf07d09a3531b4ca3c576458556c4f19ea8ace4
Lukas-JavaScript <lukaswittek96@gmail.com>
2026-02-07 06:38:36 -0500

    Preprocessed

--- automatations/auto_all_compile.sh
+++ automatations/auto_all_compile.sh
@@ -3,5 +3,9 @@ g++ main.cpp components/init.cpp components/commit.cpp components/push.cpp -o ma
 g++ main.cpp components/init.cpp components/commit.cpp components/push.cpp -o Linux/main && \
 x86_64-w64-mingw32-g++ main.cpp components/init.cpp components/commit.cpp components/push.cpp -o windows/64-bit/main.exe  -static-libgcc -static-libstdc++  && \
 i686-w64-mingw32-g++ main.cpp components/init.cpp components/commit.cpp components/push.cpp -o windows/32-bit/main.exe  -static-libgcc -static-libstdc++  && \
+g++ -E main.cpp -o build/Preprocessed/main.i && \
+g++ -E components/init.cpp -o build/Preprocessed/init.i && \
+g++ -E components/commit.cpp -o build/Preprocessed/commit.i && \
+g++ -E components/push.cpp -o build/Preprocessed/push.i && \
 cd automatations && \
 echo "Compilation complete" 
